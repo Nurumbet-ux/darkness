f7cbf93290c6180d0535f48212fe172115bcba3c
Nurumbet-ux <saryevnur@gmail.com>
2020-12-16 14:48:54 +0600

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -11,15 +9,14 @@ public class Main {
         ArrayList<String> A = new ArrayList<>();
         ArrayList<String> B = new ArrayList<>();
         ArrayList<String> C = new ArrayList<>();
-        Iterator iterator = A.iterator();
-        Iterator iterator1 = B.iterator();
-        Iterator iterator2 = C.iterator();
         int i = 0;
+        System.out.println("Create A list!..");
         while (i < 5) {
             System.out.println("add the " + i + " item: ");
             A.add(s.next());
             i++;
         }
+        System.out.println("Create B List!..");
 
         int k = 0;
         while (k < 5) {
@@ -29,27 +26,39 @@ public class Main {
         }
 
         System.out.println("The A list!\n________________");
+        Iterator <String> iterator = A.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
 
         System.out.println("The B list\n_________________________");
+        Iterator <String> iterator1 = B.iterator();
         while (iterator1.hasNext()) {
             System.out.println(iterator1.next());
         }
 
         iterator = A.iterator();
         iterator1 = B.iterator();
-        while (iterator.hasNext() || iterator1.hasNext()) {
-            C.add((String) iterator.next());
-            C.add((String) iterator1.next());
+        while (iterator.hasNext() && iterator1.hasNext()) {
+            C.add(iterator.next());
+            C.add(iterator1.next());
         }
 
         System.out.println("The C list\n_____________________");
+        Iterator <String> iterator2 = C.iterator();
         while (iterator2.hasNext()) {
             System.out.println(iterator2.next());
         }
+        System.out.println("Sorted list!..");
+        System.out.println("_______________________");
+        C.sort(Comparator.comparing(String :: length));
+        iterator2 = C.iterator();
+        while (iterator2.hasNext()){
+            System.out.println(iterator2.next());
+        }
+
 
     }
+
 
 }
